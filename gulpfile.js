61135424d8fe28ab1e8cdf6b22f4ed38deb594ed
Asael Jaimes <asaelx@gmail.com>
2016-11-07 13:47:29 -0600

    Install Gentelella Template

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,43 @@ require('laravel-elixir-vue-2');
  |
  */
 
+// Gentelella vendors path : vendor/bower_components/gentelella/vendors
+
 elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+
+    /********************/
+    /* Copy Stylesheets */
+    /********************/
+
+    // Bootstrap
+    mix.copy('vendor/bower_components/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
+
+    // Font awesome
+    mix.copy('vendor/bower_components/gentelella/vendors/font-awesome/css/font-awesome.min.css', 'public/css/font-awesome.min.css');
+
+    // Gentelella
+    mix.copy('vendor/bower_components/gentelella/build/css/custom.min.css', 'public/css/gentelella.min.css');
+
+    /****************/
+    /* Copy Scripts */
+    /****************/
+
+    // Bootstrap
+    mix.copy('vendor/bower_components/gentelella/vendors/bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js');
+
+    // jQuery
+    mix.copy('vendor/bower_components/gentelella/vendors/jquery/dist/jquery.min.js', 'public/js/jquery.min.js');
+
+    // Gentelella
+    mix.copy('vendor/bower_components/gentelella/build/js/custom.min.js', 'public/js/gentelella.min.js');
+
+    /**************/
+    /* Copy Fonts */
+    /**************/
+
+    // Bootstrap
+    mix.copy('vendor/bower_components/gentelella/vendors/bootstrap/fonts/', 'public/fonts');
+
+    // Font awesome
+    mix.copy('vendor/bower_components/gentelella/vendors/font-awesome/fonts/', 'public/fonts');
 });
